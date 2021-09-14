bd39a9bb4dda1942d9dc329328e56dbc15c23d46
Aman Dixit <amandikshit10@gmail.com>
2021-09-14 16:33:11 +0530

    add

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express=require('express')
 
 
 const app=express();
-
+app.set('view engine','ejs');
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
+    const blogs=[
+        {title: "This is blog 1", snippet:"This is snippet one"},
+        {title: "This is blog two", snippet:"This is snippet two"}
+    ];
+    res.render('index',{title: 'Home', blogs});  // title is set to home and blogs is passes tot the page
 });
 app.get('/about',(req,res)=>{
-    res.sendFile(__dirname+'/about.html')
+   res.render('about');
+   res.end();
 });
-app.get('/about-me',(req,res)=>{
-    res.redirect('/');
-    res.end();
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about');
+});
+app.get('/blogs/create',(req,res)=>{
+    res.render('create');
+});
+app.use((req,res)=>{
+    res.status(404).render('404');
 });
 app.listen(3000);
