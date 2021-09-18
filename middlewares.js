05c69fecca626494e4ddd09c83903e509d47f439
Aman Dixit <amandikshit10@gmail.com>
2021-09-18 16:46:56 +0530

    add

--- middlewares.js
+++ middlewares.js
@@ -5,6 +5,13 @@ const app=express();
 
 app.use('/xyz', express.static(__dirname+'/public'))
 
+app.get('/' , (req,res)=>{
+    if(req.query.name)
+    {
+        console.log("this is my name -- "+req.query.name)
+    }
+    res.send("Hello "+req.query.name)
+})
 app.listen(3000,()=>{
     console.log('listning the request')
 })
